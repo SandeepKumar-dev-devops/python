e7dbe18e39ed9bb72ab5a13578907dc68578bda9
SandeepKumar-dev-devops <kalashkamsandeepkumar@gmail.com>
2025-05-30 08:04:38 +0530

    add new files

---
+++ multiplicatin.py
@@ -0,0 +1,7 @@
+#multiplication of two number
+
+
+x=9
+y=4
+z=x*y
+print('Multiplication of two number is',{z})
---
+++ sub.py
@@ -0,0 +1,6 @@
+# substraction of two numbers
+
+x=57
+y=97
+z=x-y
+print('substraction of Two numbe is',z)
